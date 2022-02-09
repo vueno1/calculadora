891044895f1555ea7dcd13072cb603e1465beebb
vueno <uenovale@gmail.com>
2022-02-09 14:10:18 -0300

    calculadora, final del codigo

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const botonesOperadores = document.querySelectorAll ('.operador')
 CLASES
 /////*/
 
+//declaramos la clase Calculadora y sus metodos (+,-,div, mult)
 class Calculadora {
 
     //metodos ↓↓↓
@@ -41,9 +42,10 @@ class Calculadora {
     }
 }
 
-//clase controla la calculadora
+//declaramos la clase DISPLAY y su constructor
 class Display {
 
+    //creo un objeto con sus diferentes propiedades
     constructor (displayValorAnterior, displayValorActual) {
         //propiedades del display = 
 
@@ -53,34 +55,109 @@ class Display {
         //propiedad calculadora
         this.calculadora = new Calculadora ()
 
-        //valor actual (numero que estoy gurdando)
+        this.tipoOperacion = undefined //las operaciones (botones) se van a ir guardando aqui.
+
+        //valor actual (numero que estoy guardando)
         this.valorActual = '' //usamos strings, y no numeros
         
         //valor anterior (numero que estoy guardando)
         this.valorAnterior = ''
 
+        //creamos los signos para agregar al display
+        this.signos = {
+            sumar:"+",
+            dividir: "%",
+            multiplicar: "x",
+            restar: "-"
+        }
+
     }
 
+    //metodo 1)
     agregarNumero (numero) {
 
         //para que no me repita el punto escribo una condicion: 
-        if (numero === '.' && this.valorActual.includes ('.')) return
+        if (numero === '.' && this.valorActual.includes ('.')) return //si el numero es = "." y ademas ya hay un "." incluido, 
+        //no haga nada con ese boton.
         
-        this.valorActual = this.valorActual + numero
-        this.imprimirValores()
+        //si eso no se cumple, me va agregando mas numeros despues del punto
+        this.valorActual = this.valorActual.toString() + numero.toString() //al valor actual le sumamos el numero elegido
+        //se usa .toString porque vamos a estar usando strings y numeros.
+
+        this.imprimirValores() //agrego la funcion para imprimir los valores.
     }
 
+    //metodo 2)
     //metodo para imprimir los numeros segun bootn que toque.
     imprimirValores () {
-        this.displayValorActual.textContent = this.valorActual
-        this.displayValorAnterior.textContent = this.valorAnterior
+        this.displayValorActual.textContent = this.valorActual //el contenido del display le imprimo el valor actual 
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}` 
+        //al valor anterior le digo que es igual a el valor anterior + el signo de operacion elegida
+        //si no tiene ninguno de estos signos, que no ponga nada al final ("")
     }
+
+    //metodo 3)
+    borrar () {
+        this.valorActual = this.valorActual.toString().slice (0,-1) //corta el ultimo numero
+        this.imprimirValores () //y actualizo los valores en display
+        //recuerda: agregar onclick al boton en html
+    }
+
+    //metodo 4) 
+    borrarTodo () {
+        this.valorActual = '' //se borra el valor actual
+        this.valorAnterior = '' // se borra el valor anterior
+        this.tipoOperacion = undefined // se borra cualquier operacion de boton
+        this.imprimirValores () // y lo imprimo
+        //recuerda: agregar onclick en boton 
+    }
+
+    //metodo 5)
+    calcular () {
+        
+        //traemos los valores de valor actual y anterior y los guardamos en variables
+        const valorAnterior = parseFloat (this.valorAnterior)
+        const valorActual = parseFloat (this.valorActual)
+        //ambos guardados como numeros con el parsefloat
+
+        //si quiere hacer un parseFloate de una variable vacia (``) entonces me tira un NAN
+        //si cualquiera de estas variables son NAN entonces que no haga nada.
+        if (isNaN(valorActual)|| isNaN(valorAnterior)) return
+ 
+        //si cualqueir de las variables tiene un valor numerico entonces hacemos lo siguiente:
+        //usamos un metodo de la calculadora, y a traves del operador elegido calculamos el valor actual entre (parametros)
+        this.valorActual = this.calculadora [this.tipoOperacion] (valorAnterior, valorActual)
+    }
+
+    //metodo 6)
+    computar (tipo) {
+        this.tipoOperacion !== "igual" && this.calcular() //si el tipo de operador que elegimos es "=" solo entonces hacemos la operacion calcular
+        
+        this.tipoOperacion = tipo //el tipo de operacion se actualiza al que acabamos de recibir (tipo)
+        this.valorAnterior = this.valorActual || this.valorAnterior //si hacemos el calculo y el valor anterior cambia, entonces 
+        //se muestra el valor actual, sino queda el valor anterior (es decir, si seguimos calculando el valor anterior siempre queda, 
+        //si elegimos = entonces el valor anterior cambia x el actual)
+        this.valorActual = "" //cuando terminamos la operacion el valor actual queda vacio.
+        this.imprimirValores () //siempre imprimimos para que muestre el calculo final.
+    }
+
 }
 
+//creo la constante display que como valor tiene el objeto creado.
 const display = new Display (displayValorAnterior, displayValorActual)
 
+//TODOS LOS BOTONES DE NUMEROS HACEN ESTO AL HACER CLICK
 botonesNumeros.forEach (boton =>{
     boton.addEventListener ('click', ()=>{
-        display.agregarNumero (boton.innerHTML)
+        display.agregarNumero (boton.innerHTML) //por cada click, agrego ese valor a display.
+        //usamos el innerhtml porque tomamos el numero escrito en el html para poder guardarlo en js.
+    })
+})
+
+//TODOS LOS BOTONES DE OPERADORES HACEN ESTO AL HACER CLICK
+botonesOperadores.forEach (boton => {
+    boton.addEventListener ('click', ()=>{
+        display.computar (boton.value) //por cada boton de operador que apretemos mandamos una funcion "computar" al display con un valor
+        //usamos el valor y no el innerHTML porque lo que necesitamos no es el (%, + , - o el X) sino el VALUE de ese <div>
     })
 })
